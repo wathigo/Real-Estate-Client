c0877dbccba0bc4f70668dc2a6a89996a92fa5c5
Simon Wathigo <wathigosimon@gmail.com>
2020-06-26 11:09:28 +0300

    Add log out functionality, add more styles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/HomePage';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <HomePage/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>Project by Simon Wathigo @2020</p>
+        </footer>
+    )
+}
+
+export default Footer;
